41729933a47b112898a88c916951d25a619d60fb
김진호 <rlawlsgh1227@gmail.com>
2024-05-24 20:37:37 +0900

    chore: 배포 설정

--- src/router.tsx
+++ src/router.tsx
@@ -5,33 +5,36 @@ import { ROUTER_URLS } from './constants/constants';
 import { ShoppingCartOverview, OrderInfo, PaymentInfo, ProductList } from '@pages/index.ts';
 import InvalidAccessFallback from '@components/fallback/InvalidAccessFallback/InvalidAccessFallback.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        path: ROUTER_URLS.MAIN,
-        element: <ShoppingCartOverview />,
-      },
-      {
-        path: ROUTER_URLS.ORDER_INFO,
-        element: <OrderInfo />,
-        errorElement: <InvalidAccessFallback />,
-      },
-      {
-        path: ROUTER_URLS.PAYMENT_INFO,
-        element: <PaymentInfo />,
-        errorElement: <InvalidAccessFallback />,
-      },
-      {
-        path: ROUTER_URLS.ADMIN,
-        element: <ProductList />,
-      },
-    ],
-    errorElement: <InvalidAccessFallback />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          path: ROUTER_URLS.MAIN,
+          element: <ShoppingCartOverview />,
+        },
+        {
+          path: ROUTER_URLS.ORDER_INFO,
+          element: <OrderInfo />,
+          errorElement: <InvalidAccessFallback />,
+        },
+        {
+          path: ROUTER_URLS.PAYMENT_INFO,
+          element: <PaymentInfo />,
+          errorElement: <InvalidAccessFallback />,
+        },
+        {
+          path: ROUTER_URLS.ADMIN,
+          element: <ProductList />,
+        },
+      ],
+      errorElement: <InvalidAccessFallback />,
+    },
+  ],
+  { basename: '/react-shopping-cart/' },
+);
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-shopping-cart/',
   resolve: {
     alias: [
       { find: '@api', replacement: path.resolve(__dirname, 'src/api') },
